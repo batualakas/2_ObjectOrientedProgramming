d2df0f089da0bcbea0110ee910f8081e000e24c8
batualakas <batualakas@gmail.com>
2021-09-22 13:55:48 +0300

    ii

---
+++ k9_StaticConst/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace k9_StaticConst
+{
+    /*
+     static keyword : Eğer bir sınıf static diye işaretlenirse bu sınıftan instance alınamaz. Ram üzerine bir kez çıkmasını sağlayan anahtar kelimedir. Bu sınıf içinde tanımlanan tüm metot ve ya propertler de static tanımlanmak zorundadır. Kullanılmak istendiğinde SinifAdi.PropertAdi ve ya SinifAdi.MetotAdi() şeklinde kullanım yapılır.
+     ***** STATİC kullanırken 3 kez düşünün. 
+     ** Garbage collector.
+     * const keyword : Sabit bir değer tanımlamak için kullanılır ve değeri sonrada değiştirilemez.
+
+*/
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            var eulerSabiti = Matematik.EulerSabiti;
+            // Helpers : 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // var sonuc = Matematik.KarelerFarkiniAl(2, 3);
+            // MessageBox.Show($"{sonuc}");
+            
+            
+        }
+    }
+}
---
+++ k9_StaticConst/Matematik.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace k9_StaticConst
+{
+    public static class Matematik
+    {
+        // public static double EulerSabiti = 0.57721; // bu sabitin değerini değiştirilebilir olsun istemiyoruz. o yüzden buna const ekleyeceğiz. Static olursa değiştirilebilir ama const olursa değiştirelemez.
+        public const double EulerSabiti = 2.7182818;
+
+        public const double PisagorSabiti = 1.4142135;
+
+        public static double KarelerFarkiniAl(double sayi1, double sayi2)
+        {
+            var sayi1Karesi = Math.Pow(sayi1, 2);
+            var sayi2Karesi = Math.Pow(sayi2, 2);
+            var karelerFarki = sayi1Karesi - sayi2Karesi;
+            return karelerFarki;
+            // bu static bir metot oldu. yani içerdeki değerler değiştirilebilir fakat ram üstene 1 kez gelir yazılır ve biter.
+        }
+    }
+}
